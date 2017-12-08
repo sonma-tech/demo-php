816fce254b0d09d333487a007086c8eee58823fb
放荡不羁爱自由 <admin@wansir.com>
2017-12-08 14:44:49 +0800

    Update SDK.php

--- SDK.php
+++ SDK.php
@@ -9,7 +9,7 @@ class SDK {
   public function __construct($accessKey, $secretKey) {
     $this->accessKey = $accessKey;
     $this->secretKey = $secretKey;
-    $this->host = "https://api.sonma.net";
+    $this->host = "http://api.sonma.net";
   }
 
   /**
@@ -22,14 +22,63 @@ class SDK {
    * link http://docs.sonma.net
    */
 
-  public function print($sn,$data,$template) {
-      return $this->request('POST','/v1/print',array('sn' => $sn,'content' => json_encode($data),'template' => $template));
+  public function print1($sn,$data,$template) {
+      return $this->curl('POST','/v1/print',array('sn' => $sn,'content' => json_encode($data),'template' => $template));
   }
   
-  private function request($method, $api, $content) {
+  private function curl($method, $api, $content) {
       
     ksort($content); //sort by key asc
-    $requestBody = http_build_query($content); //RFC3986 URL Encode
+    //$requestBody = http_build_query($content,null,'&',PHP_QUERY_RFC3986);//RFC3986 URL Encode
+    $requestBody  = str_replace('+', '%20', http_build_query($content,null,'&'));
+    $timeStamp = time();//unix timestamp
+    $hashedQueryString = sha1($requestBody);//hex(sha1(*))
+    $stringToSign = "{$timeStamp}\n".$hashedQueryString;//mixed
+    $signature = hash_hmac('sha1',$stringToSign,$this->secretKey);//hex(hmac_sha1(*))
+    $authorization = base64_encode("HMAC-SHA1 {$this->accessKey}:{$signature}");//base64_encode
+
+    // echo "规范查询字符串:{$requestBody}<br>".
+    //      "规范查询字符串哈希:{$hashedQueryString}<br>".
+    //      "待签字符串:{$stringToSign}<br>".
+    //      "签名:{$signature}<br>".
+    //      "鉴权字符串:{$authorization}<br>";
+    
+    
+      $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $this->host.$api,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $requestBody,
+      CURLOPT_HTTPHEADER => array(
+        "Content-type:application/x-www-form-urlencoded",
+        "Timestamp:{$timeStamp}",  
+        "Authorization:{$authorization}"
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    
+    curl_close($curl);
+    
+    if ($err) {
+      return $err;
+    } else {
+      return $response;
+    }
+  }
+  
+  private function post($method, $api, $content) {
+      
+    ksort($content); //sort by key asc
+    //$requestBody = http_build_query($content,null,'&',PHP_QUERY_RFC3986);//RFC3986 URL Encode
+    $requestBody  = str_replace('+', '%20', http_build_query($content,null,'&'));
     $timeStamp = time();//unix timestamp
     $hashedQueryString = sha1($requestBody);//hex(sha1(*))
     $stringToSign = "{$timeStamp}\n".$hashedQueryString;//mixed
